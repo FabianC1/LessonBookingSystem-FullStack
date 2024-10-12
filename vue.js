e46cab38287db32cc572c305a5944ff6cdc4cfc2
Fabian <galaselfabian@gmail.com>
2024-10-12 13:40:32 +0100

    added styling to sorting function and added duration for each class

--- vue.js
+++ vue.js
@@ -3,16 +3,16 @@ let app = new Vue({
     data: {
         sitename: "Lesson Booking",
         lessons: [
-            { id: 1, subject: "Math", location: "Room 101", price: 50, spaces: 3, image: "images/maths.png" },
-            { id: 2, subject: "Science", location: "Room 102", price: 60, spaces: 5, image: "images/science.jpg" },
-            { id: 3, subject: "History", location: "Room 103", price: 40, spaces: 2, image: "images/history.jpeg" },
-            { id: 4, subject: "English", location: "Room 104", price: 45, spaces: 0, image: "images/english.png" },
-            { id: 5, subject: "Art", location: "Room 105", price: 55, spaces: 4, image: "images/art.jpg" },
-            { id: 6, subject: "Music", location: "Room 106", price: 70, spaces: 1, image: "images/music.png" },
-            { id: 7, subject: "Physics", location: "Room 107", price: 65, spaces: 5, image: "images/physics.png" },
-            { id: 8, subject: "Chemistry", location: "Room 108", price: 50, spaces: 0, image: "images/chemistry.jpeg" },
-            { id: 9, subject: "Biology", location: "Room 109", price: 60, spaces: 2, image: "images/biology.png" },
-            { id: 10, subject: "Geography", location: "Room 110", price: 50, spaces: 3, image: "images/geography.jpg" }
+            { id: 1, subject: "Math", location: "Room 101", price: 50, spaces: 3, image: "images/maths.png", duration: 2 },
+            { id: 2, subject: "Science", location: "Room 102", price: 60, spaces: 5, image: "images/science.jpg", duration: 1.5 },
+            { id: 3, subject: "History", location: "Room 103", price: 40, spaces: 2, image: "images/history.jpeg", duration: 1 },
+            { id: 4, subject: "English", location: "Room 104", price: 45, spaces: 0, image: "images/english.png", duration: 2 },
+            { id: 5, subject: "Art", location: "Room 105", price: 55, spaces: 4, image: "images/art.jpg", duration: 2.5 },
+            { id: 6, subject: "Music", location: "Room 106", price: 70, spaces: 1, image: "images/music.png", duration: 1.5 },
+            { id: 7, subject: "Physics", location: "Room 107", price: 65, spaces: 5, image: "images/physics.png", duration: 2 },
+            { id: 8, subject: "Chemistry", location: "Room 108", price: 50, spaces: 0, image: "images/chemistry.jpeg", duration: 1.5 },
+            { id: 9, subject: "Biology", location: "Room 109", price: 60, spaces: 2, image: "images/biology.png", duration: 1 },
+            { id: 10, subject: "Geography", location: "Room 110", price: 50, spaces: 3, image: "images/geography.jpg", duration: 2 }
         ],
         sortAttribute: "subject", // Default sort by subject
         sortOrder: "ascending" // Default order
@@ -21,7 +21,7 @@ let app = new Vue({
         sortedLessons() {
             return this.lessons.slice().sort((a, b) => {
                 let modifier = this.sortOrder === "ascending" ? 1 : -1;
-                if (this.sortAttribute === "price" || this.sortAttribute === "spaces") {
+                if (this.sortAttribute === "price" || this.sortAttribute === "spaces" || this.sortAttribute === "duration") {
                     // Sort by numerical values
                     return (a[this.sortAttribute] - b[this.sortAttribute]) * modifier;
                 } else {
