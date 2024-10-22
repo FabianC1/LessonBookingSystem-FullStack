7332ef3941d38272500ec6d7291331dc9af5c107
Fabian Galasel <galaselfabian@gmail.com>
2024-10-22 12:57:57 +0100

    added backend

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+
+var app = express();
+
+
+
+
+app.use(express.static('Static'));
+
+
+app.use(function (req, res) {
+   res.status(404);
+   res.send("File not found!");
+});
+app.listen(3000, function () {
+   console.log("App started on port 3000");
+});
